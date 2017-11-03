818d72a042d12a709f7074c2ea68fcc10f83cee6
Bree Cummins <breecummins@gmail.com>
2017-11-03 10:30:33 -0600

    added new networks

--- Figure4/Compute.sh
+++ Figure4/Compute.sh
@@ -21,6 +21,8 @@ python ../Query/Enqueue.py computations networks/network_8 S E2F $1
 python ../Query/Enqueue.py computations networks/network_2a8 S E2F $1
 python ../Query/Enqueue.py computations networks/network_2b8 S E2F $1
 python ../Query/Enqueue.py computations networks/network_2a2b8 S E2F $1
+python ../Query/Enqueue.py computations networks/network_neither7nor8 S E2F $1
+python ../Query/Enqueue.py computations networks/network_7p27 S E2F $1
 python ../Query/Enqueue.py computations networks/network_2a S E2F $1
 python ../Query/Enqueue.py computations networks/network_2b S E2F $1
 python ../Query/Enqueue.py computations networks/network_2a2b S E2F $1
