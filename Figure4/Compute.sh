1e77f588807aecb85171a84e98bf63f5156c324a
Bree Cummins <breecummins@gmail.com>
2017-11-07 12:39:25 -0700

    uncommented other networks

--- Figure4/Compute.sh
+++ Figure4/Compute.sh
@@ -13,17 +13,17 @@
 
 mkdir -p computations
 
-# python ../Query/Enqueue.py computations networks/network_7 S E2F Rb $1
-# python ../Query/Enqueue.py computations networks/network_2a7 S E2F Rb $1
-# python ../Query/Enqueue.py computations networks/network_2b7 S E2F Rb $1
-# python ../Query/Enqueue.py computations networks/network_2a2b7 S E2F Rb $1
-# python ../Query/Enqueue.py computations networks/network_8 S E2F Rb $1
-# python ../Query/Enqueue.py computations networks/network_2a8 S E2F Rb $1
-# python ../Query/Enqueue.py computations networks/network_2b8 S E2F Rb $1
-# python ../Query/Enqueue.py computations networks/network_2a2b8 S E2F Rb $1
+python ../Query/Enqueue.py computations networks/network_7 S E2F Rb $1
+python ../Query/Enqueue.py computations networks/network_2a7 S E2F Rb $1
+python ../Query/Enqueue.py computations networks/network_2b7 S E2F Rb $1
+python ../Query/Enqueue.py computations networks/network_2a2b7 S E2F Rb $1
+python ../Query/Enqueue.py computations networks/network_8 S E2F Rb $1
+python ../Query/Enqueue.py computations networks/network_2a8 S E2F Rb $1
+python ../Query/Enqueue.py computations networks/network_2b8 S E2F Rb $1
+python ../Query/Enqueue.py computations networks/network_2a2b8 S E2F Rb $1
 python ../Query/Enqueue.py computations networks/network_neither7nor8 S E2F Rb $1
-# python ../Query/Enqueue.py computations networks/network_7p27 S E2F Rb $1
+python ../Query/Enqueue.py computations networks/network_7p27 S E2F Rb $1
 python ../Query/Enqueue.py computations networks/network_2a S E2F Rb $1
 python ../Query/Enqueue.py computations networks/network_2b S E2F Rb $1
 python ../Query/Enqueue.py computations networks/network_2a2b S E2F Rb $1
-# python ../Query/Enqueue.py computations networks/network_yeaststart S SBF SBF_Whi5 $1
+python ../Query/Enqueue.py computations networks/network_yeaststart S SBF SBF_Whi5 $1
