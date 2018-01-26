f4c0098dcf28e3b5ac4c86d356f56e65bca11e8a
Shaun Harker <sharker81@gmail.com>
2018-01-26 04:45:06 -0500

    bug fix

--- Query/ComputeQuery.py
+++ Query/ComputeQuery.py
@@ -162,7 +162,7 @@ def topological_sort(graph):
     Return list of vertices in topologically sorted order
     """
     result = []
-    dfs_stack = [ (v,0) for v in graph.vertices()]
+    dfs_stack = [ (v,0) for v in graph.vertices]
     while dfs_stack:
         (v,i) = dfs_stack.pop()
         if (v,i) in explored: continue
