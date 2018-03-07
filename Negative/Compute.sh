40c9fb10e2de09793d9e59424d8096c3194ed6cf
Shaun Harker <sharker81@gmail.com>
2018-03-07 16:26:05 -0500

    fixed typo

--- Negative/Compute.sh
+++ Negative/Compute.sh
@@ -17,36 +17,36 @@ ENQUEUE=../Query/ReverseEnqueue.py
 mkdir -p computations
 
 mkdir -p computations/CycD_E2F
-for netspec in `ls networks CycD_E2F`; do
+for netspec in `ls networks/CycD_E2F`; do
   python $ENQUEUE computations/CycD_E2F networks/CycD_E2F/${netspec} CycD E2F $1
 done
 
 mkdir -p computations/Myc_E2F
-for netspec in `ls networks Myc_E2F`; do
+for netspec in `ls networks/Myc_E2F`; do
   python $ENQUEUE computations/Myc_E2F networks/Myc_E2F/${netspec} Myc E2F $1
 done
 
 mkdir -p computations/Rb_E2F
-for netspec in `ls networks Rb_E2F`; do
+for netspec in `ls networks/Rb_E2F`; do
   python $ENQUEUE computations/Rb_E2F networks/Rb_E2F/${netspec} Rb E2F $1
 done
 
 mkdir -p computations/EE_EE
-for netspec in `ls networks EE_EE`; do
+for netspec in `ls networks/EE_EE`; do
   python $ENQUEUE computations/EE_EE networks/EE_EE/${netspec} EE EE $1
 done
 
 mkdir -p computations/MD_EE
-for netspec in `ls networks MD_EE`; do
+for netspec in `ls networks/MD_EE`; do
   python $ENQUEUE computations/MD_EE networks/MD_EE/${netspec} MD EE $1
 done
 
 mkdir -p computations/Rb_EE
-for netspec in `ls networks Rb_EE`; do
+for netspec in `ls networks/Rb_EE`; do
   python $ENQUEUE computations/RB_EE networks/Rb_EE/${netspec} Rb EE $1
 done
 
 mkdir -p computations/SBF_Whi5_SBF
-for netspec in `ls networks SBF_Whi5_SBF`; do
+for netspec in `ls networks/SBF_Whi5_SBF`; do
   python $ENQUEUE computations/SBF_Whi5_SBF networks/SBF_Whi5_SBF/${netspec} SBF_Whi5 SBF $1
 done
